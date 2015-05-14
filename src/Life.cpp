2ca33a60210115329b83c819282af1abe022d582
Anton Fedchin <afedchin@ruswizards.com>
2015-05-14 15:56:15 +0300

    [win32] Added support of DirectX 11 for Windows.

--- src/Life.cpp
+++ src/Life.cpp
@@ -25,6 +25,9 @@
 #include "Life.h"
 #include "types.h"
 #include <memory.h>
+#ifdef WIN32
+#include <d3d11.h>
+#endif
 
 struct Cell
 {
@@ -39,6 +42,12 @@ struct Cell
 #define COLOR_TIME 0
 #define COLOR_COLONY 1
 #define COLOR_NEIGHBORS 2
+
+#ifdef WIN32
+ID3D11DeviceContext* g_pContext = nullptr;
+ID3D11Buffer*        g_pVBuffer = nullptr;
+ID3D11PixelShader*   g_pPShader = nullptr;
+#endif
 
 struct Grid
 {
@@ -245,6 +254,12 @@ void reducePalette()
 
 void DrawGrid()
 {
+#ifdef WIN32
+  g_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+  size_t strides = sizeof(CUSTOMVERTEX), offsets = 0;
+  g_pContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &strides, &offsets);
+  g_pContext->PSSetShader(g_pPShader, NULL, 0);
+#endif
   for(int i = 0; i<grid.width*grid.height; i++ )
     if (grid.cells[i].state != DEAD)
       DrawRectangle((i%grid.width)*grid.cellSizeX,(i/grid.width)*grid.cellSizeY, 
@@ -409,23 +424,115 @@ CRGBA HSVtoRGB( float h, float s, float v )
 
 void DrawRectangle(int x, int y, int w, int h, const CRGBA& dwColour)
 {
-    //Store each point of the triangle together with it's colour
-    CUSTOMVERTEX cvVertices[] =
-    {
-        {(float) x, (float) y+h, 0.0f, 0.5, dwColour,},
-        {(float) x, (float) y, 0.0f, 0.5, dwColour,},
-    {(float) x+w, (float) y+h, 0.0f, 0.5, dwColour,},
-        {(float) x+w, (float) y, 0.0f, 0.5, dwColour,},
-    };
-
+  //Store each point of the triangle together with it's colour
+  CUSTOMVERTEX cvVertices[] =
+  {
+    {(float)     x, (float) y + h, 0.0f, dwColour,},
+    {(float)     x, (float)     y, 0.0f, dwColour,},
+    {(float) x + w, (float) y + h, 0.0f, dwColour,},
+    {(float) x + w, (float)     y, 0.0f, dwColour,},
+  };
+#ifndef WIN32
     glBegin(GL_TRIANGLE_STRIP);
     for (size_t j=0;j<4;++j)
     {
-      glColor3f(cvVertices[j].color.r/255.0, cvVertices[j].color.g/255.0, cvVertices[j].color.b/255.0);
+      glColor3f(cvVertices[j].color.r, cvVertices[j].color.g, cvVertices[j].color.b);
       glVertex2f(cvVertices[j].x, cvVertices[j].y);
     }
     glEnd();
+#else
+    D3D11_MAPPED_SUBRESOURCE res = {};
+    if (SUCCEEDED(g_pContext->Map(g_pVBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &res)))
+    {
+      memcpy(res.pData, cvVertices, sizeof(cvVertices));
+      g_pContext->Unmap(g_pVBuffer, 0);
+    }
+    g_pContext->Draw(4, 0);
+#endif
 }
+
+#ifdef WIN32
+const BYTE PixelShader[] =
+{
+     68,  88,  66,  67,  18, 124, 
+    182,  35,  30, 142, 196, 211, 
+     95, 130,  91, 204,  99,  13, 
+    249,   8,   1,   0,   0,   0, 
+    124,   1,   0,   0,   4,   0, 
+      0,   0,  48,   0,   0,   0, 
+    124,   0,   0,   0, 188,   0, 
+      0,   0,  72,   1,   0,   0, 
+     65, 111, 110,  57,  68,   0, 
+      0,   0,  68,   0,   0,   0, 
+      0,   2, 255, 255,  32,   0, 
+      0,   0,  36,   0,   0,   0, 
+      0,   0,  36,   0,   0,   0, 
+     36,   0,   0,   0,  36,   0, 
+      0,   0,  36,   0,   0,   0, 
+     36,   0,   0,   2, 255, 255, 
+     31,   0,   0,   2,   0,   0, 
+      0, 128,   0,   0,  15, 176, 
+      1,   0,   0,   2,   0,   8, 
+     15, 128,   0,   0, 228, 176, 
+    255, 255,   0,   0,  83,  72, 
+     68,  82,  56,   0,   0,   0, 
+     64,   0,   0,   0,  14,   0, 
+      0,   0,  98,  16,   0,   3, 
+    242,  16,  16,   0,   1,   0, 
+      0,   0, 101,   0,   0,   3, 
+    242,  32,  16,   0,   0,   0, 
+      0,   0,  54,   0,   0,   5, 
+    242,  32,  16,   0,   0,   0, 
+      0,   0,  70,  30,  16,   0, 
+      1,   0,   0,   0,  62,   0, 
+      0,   1,  73,  83,  71,  78, 
+    132,   0,   0,   0,   4,   0, 
+      0,   0,   8,   0,   0,   0, 
+    104,   0,   0,   0,   0,   0, 
+      0,   0,   1,   0,   0,   0, 
+      3,   0,   0,   0,   0,   0, 
+      0,   0,  15,   0,   0,   0, 
+    116,   0,   0,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   1,   0, 
+      0,   0,  15,  15,   0,   0, 
+    122,   0,   0,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   2,   0, 
+      0,   0,   3,   0,   0,   0, 
+    122,   0,   0,   0,   1,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   2,   0, 
+      0,   0,  12,   0,   0,   0, 
+     83,  86,  95,  80,  79,  83, 
+     73,  84,  73,  79,  78,   0, 
+     67,  79,  76,  79,  82,   0, 
+     84,  69,  88,  67,  79,  79, 
+     82,  68,   0, 171,  79,  83, 
+     71,  78,  44,   0,   0,   0, 
+      1,   0,   0,   0,   8,   0, 
+      0,   0,  32,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      0,   0,   3,   0,   0,   0, 
+      0,   0,   0,   0,  15,   0, 
+      0,   0,  83,  86,  95,  84, 
+     65,  82,  71,  69,  84,   0, 
+    171, 171
+};
+
+void InitDXStuff(void)
+{
+  ID3D11Device* pDevice = nullptr;
+  g_pContext->GetDevice(&pDevice);
+
+  CD3D11_BUFFER_DESC vbDesc(sizeof(CUSTOMVERTEX) * 5, D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
+  pDevice->CreateBuffer(&vbDesc, nullptr, &g_pVBuffer);
+
+  pDevice->CreatePixelShader(PixelShader, sizeof(PixelShader), nullptr, &g_pPShader);
+
+  SAFE_RELEASE(pDevice);
+}
+#endif // WIN32
 
 void SetDefaults()
 {
@@ -454,7 +561,10 @@ ADDON_STATUS ADDON_Create(void* hdl, void* props)
   g_fRatio = (float)g_iWidth/(float)g_iHeight;
   SetDefaults();
   CreateGrid();
-
+#ifdef WIN32
+  g_pContext = reinterpret_cast<ID3D11DeviceContext*>(scrprops->device);
+  InitDXStuff();
+#endif
   return ADDON_STATUS_NEED_SETTINGS;
 }
 
@@ -486,6 +596,10 @@ extern "C" void Render()
 extern "C" void ADDON_Stop()
 {
   delete grid.fullGrid;
+#ifdef WIN32
+  SAFE_RELEASE(g_pPShader);
+  SAFE_RELEASE(g_pVBuffer);
+#endif
 }
 
 extern "C" void ADDON_Destroy()
